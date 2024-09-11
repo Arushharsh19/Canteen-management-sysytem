f963939d2eaccdf4a054759595a01adeebfc32c6
Arush <a2022cse8915@imsec.ac.in>
2024-09-12 01:39:50 +0530

    First

---
+++ about_us.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About US</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <style type="text/css">
+       body{
+       background-image: url("images/bg3.jpg");
+       background-size: 100%;
+       background-repeat: no-repeat;
+
+    }
+    .dp{
+        height:400px;
+        position: relative;
+        text-align:center;
+      }
+      .d1{
+        position: absolute;
+        left:10px;
+        top:80px;
+      }
+      .d2{
+        position: absolute;
+        left:315px;
+        top:80px;
+      }
+      .d3{
+        position: absolute;
+        left:620px;
+        top:80px;
+      }
+      .d4{
+        position: absolute;
+        left:925px;
+        top:80px;
+      }
+      .d5{
+        position: absolute;
+        left:1230px;
+        top:80px;
+      }
+
+
+
+
+    
+    </style>
+</head>
+<body>
+  <h1 align="center" style="color: black">THE TEAM</h1>
+
+  
+
+
+  <div class="dp" style="color:white">
+    <div class="d1">
+      <img class="img-rounded " src="images/nitin.png" alt="Nitin" style="width: 300px;height: 300px">
+      <h3>Nitin Gangwar</h3>
+      <p> Developer</p>
+    </div>
+    <div class="d2">
+      <img class="img-rounded " src="images/arush.jpg" alt="Arush" style="width: 300px;height: 300px">
+      <h3>Arush Gupta</h3>
+      <p>Back End Developer</p>
+    </div>
+    <div class="d3">
+      <img class="img-rounded " src="images/siddhi.png" alt="Siddhi" style="width: 300px;height: 300px">
+      <h3>Siddhi Gupta</h3>
+      <p>Front End Developer</p>
+    </div>
+    <div class="d4">
+      <img class="img-rounded " src="images/saumya.png" alt="Saumya" style="width: 300px;height: 300px">
+      <h3>Saumya Singh</h3>
+      <p>Front End Developer</p>
+    </div>
+    <div class="d5">
+      <img class="img-rounded " src="images/naman.png" alt="Naman" style="width: 300px;height: 300px">
+      <h3>Naman Gupta</h3>
+      <p>Front End Developer</p>
+    </div>
+  
+</body>
+</html>
+
+

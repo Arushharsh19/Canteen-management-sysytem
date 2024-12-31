087f0b0990275d35d2ef2214ed2d56d4ad9922bd
Arush Gupta <a2022cse8915@imsec.ac.in>
2024-12-31 15:25:39 +0530

    Update ecommerce.sql

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 02, 2017 at 06:19 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.10
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
